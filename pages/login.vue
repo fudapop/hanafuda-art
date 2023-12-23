25579227a048b14e63c0be90e51b9531f6928373
Andre Hammons <dev@ahammons.me>
2023-12-23 12:28:04 -0800

    fix: guest-login-button id

--- pages/login.vue
+++ pages/login.vue
@@ -38,7 +38,7 @@
           </div>
 
           <div class="mt-10">
-            <button id="guest-login-buttonu" type="button" @click="handleLoginAsGuest" class="w-full tracking-wide shadow-md pri-btn">
+            <button id="guest-login-button" type="button" @click="handleLoginAsGuest" class="w-full tracking-wide shadow-md pri-btn">
               Continue as guest
             </button>
 
