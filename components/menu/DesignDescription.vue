609c796c7225d64e1054cf57ef8e5ae081194dc8
Andre Hammons <dev@ahammons.me>
2023-12-24 16:13:40 -0800

    style: sticky menu header

--- components/menu/DesignDescription.vue
+++ components/menu/DesignDescription.vue
@@ -1,19 +1,21 @@
 <template>
-	<h3 class="text-lg font-semibold tracking-wide text-gray-900 dark:text-white">{{ info.title }}</h3>
-	<p>
-	{{ info.description }}
-	</p>
-	<a v-if="info.url" :href="info.url" target="_blank"
-	class="text-indigo-600 dark:text-yellow-300 hover:text-indigo-500 dark:hover:text-yellow-100 hover:underline underline-offset-4">
-	{{ info.urlDescription }} &rarr;
-	</a>
-	<p v-if="info.contributor">
-	Contributed by 
-	<a :href="info.contributorUrl" target="_blank"
-			class="text-indigo-600 dark:text-yellow-300 hover:text-indigo-500 dark:hover:text-yellow-100 hover:underline underline-offset-4">
-			{{ info.contributor }}
-	</a>
-	</p>
+	<div class="mb-4 space-y-2">
+		<h3 class="text-lg font-semibold tracking-wide text-gray-900 dark:text-white">{{ info.title }}</h3>
+		<p>
+		{{ info.description }}
+		</p>
+		<a v-if="info.url" :href="info.url" target="_blank"
+		class="text-indigo-600 dark:text-yellow-300 hover:text-indigo-500 dark:hover:text-yellow-100 hover:underline underline-offset-4">
+		{{ info.urlDescription }} &rarr;
+		</a>
+		<p v-if="info.contributor">
+		Contributed by 
+		<a :href="info.contributorUrl" target="_blank"
+				class="text-indigo-600 dark:text-yellow-300 hover:text-indigo-500 dark:hover:text-yellow-100 hover:underline underline-offset-4">
+				{{ info.contributor }}
+		</a>
+		</p>
+	</div>
 </template>
 
 <script setup lang="ts">
