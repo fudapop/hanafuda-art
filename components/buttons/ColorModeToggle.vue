c637d9561e36e4b538a808739c24010a8a07a2e9
Andre Hammons <96357301+AhamSammich@users.noreply.github.com>
2024-05-20 02:01:45 -0700

    fix: call ref value and add comments

--- components/buttons/ColorModeToggle.vue
+++ components/buttons/ColorModeToggle.vue
@@ -1,12 +1,19 @@
 <template>
+  <!-- Color Mode Toggle Button 
+    This button toggles the color mode when clicked.
+    It cycles between dark, light, and auto modes.
+  -->
   <button
     @click="handleClick"
     title="Toggle Color Mode"
-    class="inline-flex w-full justify-center gap-x-1.5 rounded-lg px-2 py-1.5 text-sm font-semibold drop-shadow-md text-white"
+    :class="[
+      'inline-flex w-full justify-center gap-x-1.5 rounded-lg px-2 py-1.5 text-sm font-semibold ',
+      'drop-shadow-md text-white dark:hover:text-yellow-300 hover:text-indigo-600',
+    ]"
   >
     <Icon v-if="state === 'dark'" name="ic:outline-dark-mode" class="w-6 h-6 drop-shadow-sm" />
     <Icon v-else-if="state === 'light'" name="ic:outline-light-mode" class="w-6 h-6 drop-shadow-sm" />
-    <Icon v-else name="ic:round-monitor" class="w-6 h-6 drop-shadow-sm" />
+    <Icon v-else name="ic:round-monitor" class="w-6 h-6 drop-shadow-sm hover:text-yellow-300" />
     <span class="sr-only">{{ state }}</span>
   </button>
 </template>
