09b1a7ddaa00e061033ab261c0ae50a7614173e2
Andre Hammons <96357301+AhamSammich@users.noreply.github.com>
2025-07-16 03:28:59 -0700

    fix: show artist if not null

--- components/ArtistCredit.vue
+++ components/ArtistCredit.vue
@@ -1,5 +1,8 @@
 <template>
-  <span class="text-xs italic opacity-40 !pointer-events-auto dark:text-white">
+  <span
+    v-if="!!designInfo.creator"
+    class="text-xs italic opacity-40 !pointer-events-auto dark:text-white"
+  >
     <span class="cursor-default"> Cards by </span>
     <a
       v-if="designInfo.creator"
