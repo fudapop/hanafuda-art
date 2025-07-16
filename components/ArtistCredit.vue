5d785878a894308147c2186b7c12213289abb470
Andre Hammons <96357301+AhamSammich@users.noreply.github.com>
2025-07-16 02:31:27 -0700

    refactor: create ArtistCredit component

---
+++ components/ArtistCredit.vue
@@ -0,0 +1,19 @@
+<template>
+  <span class="text-xs italic opacity-40 !pointer-events-auto dark:text-white">
+    <span class="cursor-default"> Cards by </span>
+    <a
+      v-if="designInfo.creator"
+      :title="designInfo.urlDescription"
+      :href="designInfo.url"
+      target="_blank"
+      class="underline underline-offset-4 whitespace-nowrap"
+    >
+      {{ designInfo.creator }}
+    </a>
+  </span>
+</template>
+
+<script setup lang="ts">
+const { getDesignInfo, currentDesign } = useCardDesign()
+const designInfo = computed(() => getDesignInfo(currentDesign.value))
+</script>
