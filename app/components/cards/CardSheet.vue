e360c0eed6df7e7a0814c82911de67f86e7dba85
Andre Hammons <96357301+AhamSammich@users.noreply.github.com>
2025-11-10 19:15:49 -0800

    fix: update card sheet display order and trigger icon

--- app/components/cards/CardSheet.vue
+++ app/components/cards/CardSheet.vue
@@ -2,22 +2,26 @@
   <div class="p-8">
     <div :class="[currentDesign, 'grid grid-cols-4 sm:grid-cols-8 gap-0.5']">
       <CardImage
-        v-for="card in cards"
-        :card="card[0]"
-        :src="card[1]"
+        v-for="cardName in cardArray"
+        :card="cardName"
+        :src="cardMap.get(cardName)"
       />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { type CardName } from '~/utils/cards'
+
 interface Props {
   design?: CardDesign
 }
 const props = defineProps<Props>()
 
-const { currentDesign, getCardUrlMap } = useCardDesign()
-const cards = computed(() =>
-  Array.from(getCardUrlMap(props.design || currentDesign.value).entries()),
-)
+const { currentDesign, getCardUrlMap, getDesignInfo } = useCardDesign()
+const cardMap = computed(() => getCardUrlMap(currentDesign.value))
+const cardArray = computed(() => {
+  const { arrangement } = getDesignInfo()
+  return (arrangement?.orderByName ?? Array.from(cardMap.value.keys())) as CardName[]
+})
 </script>
